cb51482b043ab7cab0ac4f1ff1ebcc1c185db91e
John Armstrong-Prior <jarmstrongprior@googlemail.com>
2016-03-31 17:36:03 +0100

    Removed un-required actions and controllers. Added angular js file script tags.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
 
 namespace BowlingScorer.Controllers
@@ -10,20 +6,6 @@ namespace BowlingScorer.Controllers
     {
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
             return View();
         }
 
