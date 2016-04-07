dc40d60396bde0c25490189dbb95ef3935f81558
John Armstrong-Prior <jarmstrongprior@googlemail.com>
2016-04-07 07:30:05 +0100

    Added basic turn scoring.

--- js/app.js
+++ js/app.js
@@ -3,20 +3,43 @@
 (function() {
     var app = angular.module('bowlingScoreCard', []);
 
+    var Turn = function() {
+        this.rollScores = [];
+        this.score = '0';  
+    };
+
     app.controller('ScoreCardController', ['$scope', function($scope) {
         $scope.totalScore = 0;
         $scope.playerName = '';
         $scope.rollScores = [];
+        $scope.turns = [ ];
 
         function recalculateTotalScore () {
             $scope.totalScore = 0;
-            for(var i = 0; i < $scope.rollScores.length; i++) {
-                $scope.totalScore += $scope.rollScores[i];
+            
+            for(var turnIndex = 0; turnIndex < $scope.turns.length; turnIndex++){
+                var turnScoreValue = 0;
+                for(var rollIndex = 0; rollIndex < $scope.turns[turnIndex].rollScores.length; rollIndex++) {
+                    var rollScoreValue = new Number($scope.turns[turnIndex].rollScores[rollIndex]);
+                    $scope.totalScore += rollScoreValue;
+                    turnScoreValue += rollScoreValue;
+                }
+                $scope.turns[turnIndex].score = new String(turnScoreValue);    
             }
         };
         
-        $scope.enterPlayerScore = function(roll, score) {
-            $scope.rollScores[roll - 1] = score;
+        function addScoreToTurnRoll(turn, roll, score) {
+           while($scope.turns.length < turn + 1){
+               $scope.turns.push(new Turn());
+            }
+            while($scope.turns[turn].rollScores.length < roll + 1){
+                $scope.turns[turn].rollScores.push('0');
+            }
+            $scope.turns[turn].rollScores[roll] = score;
+        }
+        
+        $scope.enterPlayerScore = function(turn, roll, score) {
+            addScoreToTurnRoll(turn, roll, score);
             recalculateTotalScore();
         };
     
--- specs/tests.js
+++ specs/tests.js
@@ -26,14 +26,16 @@ describe("ScoreCardController", function() {
        
         it("should show total score as 5 when 5 entered", function() {
             $controller('ScoreCardController', { $scope: $scope });
-            $scope.enterPlayerScore(1, 5);
-            expect($scope.totalScore).toEqual(5);       
+            $scope.enterPlayerScore(0, 0, '5');
+            expect($scope.totalScore).toEqual(5);
+            expect($scope.turns[0].score).toEqual('5');
         });
         
         it("should show total score as 7 when 7 entered", function() {
             $controller('ScoreCardController', { $scope: $scope });
-            $scope.enterPlayerScore(1, 7);
+            $scope.enterPlayerScore(0, 0, '7');
             expect($scope.totalScore).toEqual(7);       
+            expect($scope.turns[0].score).toEqual('7');
         });
     });
     
@@ -41,16 +43,43 @@ describe("ScoreCardController", function() {
        
         it("should show total score as 7 when 5 then 2 entered", function() {
             $controller('ScoreCardController', { $scope: $scope });
-            $scope.enterPlayerScore(1, 5);
-            $scope.enterPlayerScore(2, 2);
-            expect($scope.totalScore).toEqual(7);       
+            $scope.enterPlayerScore(0, 0, '5');
+            $scope.enterPlayerScore(0, 1, '2');
+            expect($scope.totalScore).toEqual(7);  
+            expect($scope.turns[0].score).toEqual('7');
         });
        
         it("should show total score as 3 when 1 then 2 entered", function() {
             $controller('ScoreCardController', { $scope: $scope });
-            $scope.enterPlayerScore(1, 1);
-            $scope.enterPlayerScore(2, 2);
+            $scope.enterPlayerScore(0, 0, '1');
+            $scope.enterPlayerScore(0, 1, '2');
             expect($scope.totalScore).toEqual(3);       
+            expect($scope.turns[0].score).toEqual('3');
+        });
+    });
+    
+       
+    describe("Second turn and first roll score entered", function() {
+       
+        it("should show total score as 12 when 5, 2 then 5 entered", function() {
+            $controller('ScoreCardController', { $scope: $scope });
+            $scope.enterPlayerScore(0, 0, '5');
+            $scope.enterPlayerScore(0, 1, '2');
+            $scope.enterPlayerScore(1, 0, '5');
+            expect($scope.totalScore).toEqual(12);  
+            expect($scope.turns[0].score).toEqual('7');
+            expect($scope.turns[1].score).toEqual('5');
+        });
+       
+        it("should show total score as 17 when 5, 2, 5 then 5 entered", function() {
+            $controller('ScoreCardController', { $scope: $scope });
+            $scope.enterPlayerScore(0, 0, '5');
+            $scope.enterPlayerScore(0, 1, '2');
+            $scope.enterPlayerScore(1, 0, '5');
+            $scope.enterPlayerScore(1, 1, '5');
+            expect($scope.totalScore).toEqual(17);  
+            expect($scope.turns[0].score).toEqual('7');
+            expect($scope.turns[1].score).toEqual('10');
         });
     });
 });
